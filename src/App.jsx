b255b7aed9b8b1ab0024911e5ec2a7acf47576dc
Kingxtrem <tantubaykingshuk@gmail.com>
2025-02-28 01:18:34 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addtask, deletetask, updatetask } from "./crud";
-import ReduxApiSetUp from "./components/ReduxApiSetUp";
+// import ReduxApiSetUp from "./components/ReduxApiSetUp";
 import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
